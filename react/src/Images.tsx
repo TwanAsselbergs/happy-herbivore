722c003c8ec2973ba2625874d23eacdb68d3ce53
NoahMelle <noahmkamphuisen@gmail.com>
2025-02-05 10:53:40 +0100

    Added the idle screen

---
+++ react/src/Images.tsx
@@ -0,0 +1,18 @@
+export const IMAGES = [
+    {
+      id: 0,
+      imageSrc:
+        "/img/breakfast.png"
+    },
+    {
+      id: 1,
+      imageSrc:
+        "/img/lunch.png"
+    },
+    {
+      id: 2,
+      imageSrc:
+        "/img/sides.png"
+    },
+  ]
+  
