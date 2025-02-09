cdb0baa30907885a43ab700c2b3e2a56cd60da8f
TwanAsselbergs <twanasselbergs@outlook.com>
2025-02-09 21:26:07 +0100

    changed span text for correct english grammar

--- react/src/components/views/IdleScreen.tsx
+++ react/src/components/views/IdleScreen.tsx
@@ -7,103 +7,99 @@ import { View } from "../../App";
 import { IMAGES } from "./Images";
 
 const sliderVariants = {
-    incoming: (direction: number) => ({
-        x: direction > 0 ? "50%" : "-50%",
-        scale: 0.6,
-        opacity: 0,
-    }),
-    active: { x: 0, scale: 1, opacity: 1 },
-    exit: (direction: number) => ({
-        x: direction > 0 ? "-50%" : "50%",
-        scale: 0.6,
-        opacity: 0,
-    }),
+  incoming: (direction: number) => ({
+    x: direction > 0 ? "50%" : "-50%",
+    scale: 0.6,
+    opacity: 0,
+  }),
+  active: { x: 0, scale: 1, opacity: 1 },
+  exit: (direction: number) => ({
+    x: direction > 0 ? "-50%" : "50%",
+    scale: 0.6,
+    opacity: 0,
+  }),
 };
 
 const sliderTransition = {
-    duration: 1,
-    ease: [0.56, 0.03, 0.12, 1.04],
+  duration: 1,
+  ease: [0.56, 0.03, 0.12, 1.04],
 };
 
 const IdleScreen = ({
-    setCurrentView,
+  setCurrentView,
 }: {
-    setCurrentView: React.Dispatch<React.SetStateAction<View>>;
+  setCurrentView: React.Dispatch<React.SetStateAction<View>>;
 }) => {
-    const [[imageCount, direction], setImageCount] = useState([0, 0]);
-    const scrollInterval = useRef<number | null>(null);
-    const activeImageIndex = wrap(0, IMAGES.length, imageCount);
+  const [[imageCount, direction], setImageCount] = useState([0, 0]);
+  const scrollInterval = useRef<number | null>(null);
+  const activeImageIndex = wrap(0, IMAGES.length, imageCount);
 
-    const swipeToImage = (swipeDirection: number) => {
-        setImageCount((prev) => [prev[0] + swipeDirection, swipeDirection]);
+  const swipeToImage = (swipeDirection: number) => {
+    setImageCount((prev) => [prev[0] + swipeDirection, swipeDirection]);
+  };
+
+  useEffect(() => {
+    scrollInterval.current = setInterval(() => swipeToImage(1), 10000);
+
+    return () => {
+      if (scrollInterval.current) {
+        clearTimeout(scrollInterval.current);
+      }
     };
+  }, []);
 
-    useEffect(() => {
-        scrollInterval.current = setInterval(() => swipeToImage(1), 10000);
-
-        return () => {
-            if (scrollInterval.current) {
-                clearTimeout(scrollInterval.current);
-            }
-        };
-    }, []);
-
-    return (
-        <div className="flex flex-col justify-between text-xl items-center w-full h-screen bg-dark-blue">
-            <img
-                src="/img/logo_big_happy_herbivore_transparent.webp"
+  return (
+    <div className="flex flex-col justify-between text-xl items-center w-full h-screen bg-dark-blue">
+      <img
+        src="/img/logo_big_happy_herbivore_transparent.webp"
+        alt=""
+        className="w-full max-w-[300px]"
+      />
+      <div className="grow flex flex-col items-center w-full p-24 h-full">
+        <div className="relative w-full h-full">
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={imageCount}
+              custom={direction}
+              variants={sliderVariants}
+              initial="incoming"
+              animate="active"
+              exit="exit"
+              transition={sliderTransition}
+              className="w-full h-full absolute will-change-[transform,opacity] flex items-center justify-center">
+              <img
+                src={IMAGES[activeImageIndex].imageSrc}
                 alt=""
-                className="w-full max-w-[300px]"
-            />
-            <div className="grow flex flex-col items-center w-full p-24 h-full">
-                <div className="relative w-full h-full">
-                    <AnimatePresence initial={false} custom={direction}>
-                        <motion.div
-                            key={imageCount}
-                            custom={direction}
-                            variants={sliderVariants}
-                            initial="incoming"
-                            animate="active"
-                            exit="exit"
-                            transition={sliderTransition}
-                            className="w-full h-full absolute will-change-[transform,opacity] flex items-center justify-center"
-                        >
-                            <img
-                                src={IMAGES[activeImageIndex].imageSrc}
-                                alt=""
-                                className="w-full h-full object-contain aspect-square drop-shadow-2xl"
-                            />
-                        </motion.div>
-                    </AnimatePresence>
-                </div>
-            </div>
-            <div className="flex flex-col items-center pb-[12rem]">
-                <h2 className="text-white uppercase text-[4rem] font-bold text-center leading-[100%]">
-                    <span className="rotate-[-4deg] block">
-                        Healthy up your day,
-                    </span>
-                    <br />
-                    <span className="flex items-center gap-6 -mt-10">
-                        the{" "}
-                        <span className="text-lime text-[8rem] block leading-[100%]">
-                            green
-                        </span>{" "}
-                        way
-                    </span>
-                </h2>
-                <h3 className=" text-white mt-4">
-                    Happy Herbivore - Healthy in a Hurry
-                </h3>
-            </div>
-            <button
-                className="font-bold bg-white w-[80%] h-[370px] rounded-t-3xl uppercase flex items-center justify-center gap-4"
-                onClick={() => setCurrentView(View.Menu)}
-            >
-                <ShoppingBag height={40} width={40} />
-                Start ordering
-            </button>
+                className="w-full h-full object-contain aspect-square drop-shadow-2xl"
+              />
+            </motion.div>
+          </AnimatePresence>
         </div>
-    );
+      </div>
+      <div className="flex flex-col items-center pb-[12rem]">
+        <h2 className="text-white uppercase text-[4rem] font-bold text-center leading-[100%]">
+          <span className="rotate-[-4deg] block">Brighten up your day,</span>
+          <br />
+          <span className="flex items-center gap-6 -mt-10">
+            the{" "}
+            <span className="text-lime text-[8rem] block leading-[100%]">
+              green
+            </span>{" "}
+            way
+          </span>
+        </h2>
+        <h3 className=" text-white mt-4">
+          Happy Herbivore - Healthy in a Hurry
+        </h3>
+      </div>
+      <button
+        className="font-bold bg-white w-[80%] h-[370px] rounded-t-3xl uppercase flex items-center justify-center gap-4"
+        onClick={() => setCurrentView(View.Menu)}>
+        <ShoppingBag height={40} width={40} />
+        Start ordering
+      </button>
+    </div>
+  );
 };
 
 export default IdleScreen;
