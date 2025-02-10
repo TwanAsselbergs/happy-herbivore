f4f6cf032893ec53d43ec00f779b674768b2e842
TwanAsselbergs <twanasselbergs@outlook.com>
2025-02-10 12:28:46 +0100

    adjusted cancel order button so it doesn't appear on confirmation page

--- react/src/components/global/TopBar.tsx
+++ react/src/components/global/TopBar.tsx
@@ -1,6 +1,13 @@
 import { Trash2 } from "lucide-react";
+import { View } from "../../App";
 
-export default function TopBar({ cancelOrder }: { cancelOrder: () => void }) {
+export default function TopBar({
+	cancelOrder,
+	currentView,
+}: {
+	cancelOrder: () => void;
+	currentView: View;
+}) {
 	return (
 		<div className="bg-white-primary px-4 z-10 relative w-full flex items-center justify-between py-10">
 			<img
@@ -8,13 +15,15 @@ export default function TopBar({ cancelOrder }: { cancelOrder: () => void }) {
 				alt=""
 				className="w-full max-w-[170px] ml-5"
 			/>
-			<button
-				className=" h-fit px-10 py-4 rounded-full bg-red-500 text-white font-bold flex items-center gap-4 mr-4"
-				onClick={cancelOrder}
-			>
-				<Trash2 width={30} height={30} strokeWidth={2} />
-				Cancel Order
-			</button>
+			{currentView !== View.Confirmation && (
+				<button
+					className=" h-fit px-10 py-4 rounded-full bg-red-500 text-white font-bold flex items-center gap-4 mr-4"
+					onClick={cancelOrder}
+				>
+					<Trash2 width={30} height={30} strokeWidth={2} />
+					Cancel Order
+				</button>
+			)}
 		</div>
 	);
 }
