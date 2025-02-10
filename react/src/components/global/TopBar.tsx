01b206f477d28470647bd6667a4d7c15842ddfc7
NoahMelle <noahmkamphuisen@gmail.com>
2025-02-10 10:02:18 +0100

    Fixed logic when generating charts

--- react/src/components/global/TopBar.tsx
+++ react/src/components/global/TopBar.tsx
@@ -1,11 +1,17 @@
+import { Trash } from "lucide-react";
+
 export default function TopBar() {
-    return (
-        <div className="bg-white-primary px-4 z-10 relative w-full">
-            <img
-                src="/img/logo_big_happy_herbivore_transparent.webp"
-                alt=""
-                className="w-full max-w-[225px]"
-            />
-        </div>
-    );
+	return (
+		<div className="bg-white-primary px-4 z-10 relative w-full flex items-center justify-between">
+			<img
+				src="/img/logo_big_happy_herbivore_transparent.webp"
+				alt=""
+				className="w-full max-w-[225px]"
+			/>
+			<button className=" h-fit px-8 py-3 rounded-xl bg-red-400 text-white font-bold flex items-center gap-4">
+				<Trash width={25} height={25} strokeWidth={2} />
+				Cancel Order
+			</button>
+		</div>
+	);
 }
