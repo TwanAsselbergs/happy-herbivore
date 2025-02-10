1153fce97e39bd0dfae48bebacc8a0f7cde741d2
TwanAsselbergs <twanasselbergs@outlook.com>
2025-02-10 10:22:04 +0100

    changed cancel order button design, adjusted trash can size

--- react/src/components/global/TopBar.tsx
+++ react/src/components/global/TopBar.tsx
@@ -6,10 +6,10 @@ export default function TopBar() {
 			<img
 				src="/img/logo_big_happy_herbivore_transparent.webp"
 				alt=""
-				className="w-full max-w-[225px]"
+				className="w-full max-w-[200px]"
 			/>
-			<button className=" h-fit px-8 py-3 rounded-xl bg-red-400 text-white font-bold flex items-center gap-4">
-				<Trash width={25} height={25} strokeWidth={2} />
+			<button className=" h-fit px-10 py-4 rounded-xl bg-red-400 text-white font-bold flex items-center gap-4 mr-4">
+				<Trash width={30} height={30} strokeWidth={3} />
 				Cancel Order
 			</button>
 		</div>
