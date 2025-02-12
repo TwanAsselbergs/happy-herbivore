d7ff7132cef284511c150b1db8ff05517af866ad
NoahMelle <noahmkamphuisen@gmail.com>
2025-02-12 09:47:12 +0100

    Turned most popups into reusable components

--- react/src/components/global/TopBar.tsx
+++ react/src/components/global/TopBar.tsx
@@ -1,5 +1,8 @@
-import { Trash2 } from "lucide-react";
+import { Trash2, Undo2 } from "lucide-react";
 import { View } from "../../App";
+import { useState } from "react";
+import Popup from "../reusable/Popup";
+import { AnimatePresence } from "framer-motion";
 
 export default function TopBar({
 	cancelOrder,
@@ -8,22 +11,51 @@ export default function TopBar({
 	cancelOrder: () => void;
 	currentView: View;
 }) {
+	const [isShowingPopup, setIsShowingPopup] = useState(false);
+
 	return (
-		<div className="bg-white-primary px-4 z-10 relative w-full flex items-center justify-between py-10">
-			<img
-				src="/img/logo_big_happy_herbivore_transparent.webp"
-				alt=""
-				className="w-full max-w-[170px] ml-5"
-			/>
-			{currentView !== View.Confirmation && (
-				<button
-					className=" h-fit px-10 py-4 rounded-full bg-red-500 text-white font-bold flex items-center gap-4 mr-4"
-					onClick={cancelOrder}
-				>
-					<Trash2 width={30} height={30} strokeWidth={2} />
-					Cancel Order
-				</button>
-			)}
-		</div>
+		<>
+			<div className="bg-white-primary px-4 z-10 relative w-full flex items-center justify-between py-10">
+				<img
+					src="/img/logo_big_happy_herbivore_transparent.webp"
+					alt=""
+					className="w-full max-w-[170px] ml-5"
+				/>
+				{currentView !== View.Confirmation && (
+					<button
+						className=" h-fit px-10 py-4 rounded-full bg-red-500 text-white font-bold flex items-center gap-4 mr-4"
+						onClick={() => setIsShowingPopup(true)}
+					>
+						<Trash2 width={30} height={30} strokeWidth={2} />
+						Cancel Order
+					</button>
+				)}
+			</div>
+			<AnimatePresence>
+				{isShowingPopup && (
+					<Popup setIsVisible={setIsShowingPopup}>
+						<div className="flex flex-col gap-8 w-full">
+							<h2>Are you sure you want to cancel this order?</h2>
+							<div className="w-full grid grid-cols-2 gap-8 font-semibold">
+								<button
+									className="flex items-center justify-center gap-4 border-2 py-5 rounded-full"
+									onClick={() => setIsShowingPopup(false)}
+								>
+									<Undo2 />
+									No, keep ordering
+								</button>
+								<button
+									className="bg-red-500 py-5 gap-4 flex justify-center items-center rounded-full text-white"
+									onClick={cancelOrder}
+								>
+									<Trash2 />
+									Yes, cancel order
+								</button>
+							</div>
+						</div>
+					</Popup>
+				)}
+			</AnimatePresence>
+		</>
 	);
 }
---
+++ react/src/components/reusable/Popup.tsx
@@ -0,0 +1,40 @@
+import { X } from "lucide-react";
+import React, { SetStateAction } from "react";
+import { motion } from "framer-motion";
+
+export default function Popup({
+	children,
+	setIsVisible,
+}: {
+	children: React.ReactNode;
+	setIsVisible: React.Dispatch<SetStateAction<boolean>>;
+}) {
+	return (
+		<div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-20">
+			<motion.div
+				className="bg-black/50 absolute w-full h-full"
+				onClick={() => setIsVisible(false)}
+				initial={{ opacity: 0 }}
+				exit={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ type: "tween" }}
+			></motion.div>
+			<motion.div
+				initial={{ y: "100%" }}
+				exit={{ y: "100%" }}
+				animate={{ y: 0 }}
+				transition={{ type: "tween" }}
+				className="h-full w-full flex justify-center items-center"
+			>
+				<div className="bg-white-primary rounded-4xl relative z-10 p-10 w-11/12 flex flex-col items-center justify-center">
+					<div className="flex w-full justify-end">
+						<button onClick={() => setIsVisible(false)}>
+							<X width={40} height={40} />
+						</button>
+					</div>
+					{children}
+				</div>
+			</motion.div>
+		</div>
+	);
+}
